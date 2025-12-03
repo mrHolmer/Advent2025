423c95e7ccc3d3dfd08864750c2bada38f9732cb
QHolmer <qholmer@schools.nyc.gov>
2025-12-03 10:00:07 -0500

    Day 3

---
+++ Day3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class Day3 {
+    public static void main(String[] args) throws FileNotFoundException{
+        File f = new File("Day3.txt");
+        Scanner s = new Scanner(f);
+    }
+}
