5f2ffc504d7dea470753792b214f934b61f1e017
Quincy Holmer <65032461+mrHolmer@users.noreply.github.com>
2025-12-08 09:47:51 -0500

    Add files via upload

--- Day3.java
+++ Day3.java
@@ -2,8 +2,79 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 public class Day3 {
+    public static int largest = -1;
+    public static int largestIndex = -1;
+    public static int secondLargest = -1;
+    public static int secondLargestIndex = -1;
     public static void main(String[] args) throws FileNotFoundException{
+        long sum = 0;
         File f = new File("Day3.txt");
         Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String battery = s.next();
+            largest(battery);
+            secondLargest(battery);
+            int joltage = getJoltage();
+            sum += joltage;
+        }
+        System.out.println(sum);
+        sum = 0;
+        s = new Scanner(f);
+        while (s.hasNext()) {
+            sum += getBank(s.next());
+        }
+        System.out.println(sum);
+    }
+    public static long getBank(String s) {
+        String battery = "";
+        int index = -1;
+        for (int i = 12; i > 0; i--) {
+            index = maxIndex(s, i, index);
+            battery += s.substring(index, index + 1);
+        }
+        return Long.parseLong(battery);
+    }
+    public static int maxIndex(String s, int start, int end)
+    {
+        int maxIndex = s.length() - start;
+        int max = Integer.parseInt(s.substring(maxIndex, maxIndex + 1));
+        int i = maxIndex;
+        while (i > end) {
+            int possible = (Integer.parseInt(s.substring(i, i + 1)));
+            if (possible >= max) {
+                max = possible;
+                maxIndex = i;
+            }
+            i--;
+        }
+        return maxIndex;
+    }
+    public static int getJoltage() {
+        if (largestIndex < secondLargestIndex) return largest * 10 + secondLargest;
+        else return secondLargest * 10 + largest;
+    }
+    public static void largest(String s) {
+        largestIndex = -1;
+        largest = -1;
+        for (int i = 0; i < s.length(); i++) {
+            int possible = Integer.parseInt(s.substring(i, i + 1));
+            if (possible > largest) {
+                largest = possible;
+                largestIndex = i;
+            }
+        }
+    }
+    public static void secondLargest(String s) {
+        int possibleIndex = largestIndex + 1;
+        if (possibleIndex == s.length()) possibleIndex = 0;
+        secondLargest = -1;
+        while(possibleIndex < s.length()) {
+            int possible = Integer.parseInt(s.substring(possibleIndex, possibleIndex + 1));
+            if (possible > secondLargest && possibleIndex != largestIndex) {
+                secondLargest = possible;
+                secondLargestIndex = possibleIndex;
+            }
+            possibleIndex++;
+        }
     }
 }
---
+++ Day4.java
@@ -0,0 +1,67 @@
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class Day4 {
+    static String[][] arr = new String[142][142];
+    public static boolean accessible(int r, int c)
+    {
+        int rolls = 0;
+        String roll = arr[r][c];
+        if (roll.equals(".")) return false;
+        if (arr[r - 1][c - 1].equals("@")) rolls++;
+        if (arr[r - 1][c].equals("@")) rolls++;
+        if (arr[r - 1][c + 1].equals("@")) rolls++;
+        if (arr[r][c - 1].equals("@")) rolls++;
+        if (arr[r][c + 1].equals("@")) rolls++;
+        if (arr[r + 1][c - 1].equals("@")) rolls++;
+        if (arr[r + 1][c].equals("@")) rolls++;
+        if (arr[r + 1][c + 1].equals("@")) rolls++;
+        return rolls < 4;
+    }
+    public static void main(String[] args) throws FileNotFoundException{
+        File f = new File("Day4.txt");
+        Scanner s = new Scanner(f);
+        for (int i = 0; i < 142; i++) {
+            arr[0][i] = ".";
+            arr[141][i] = ".";
+        }
+        int row = 1;
+        while(s.hasNext()) {
+            String line = s.next();
+            arr[row][0] = ".";
+            for(int i = 0; i < line.length(); i++) {
+                arr[row][i + 1] = line.substring(i, i + 1);
+            }
+            arr[row][141] = ".";
+            row++;
+        }
+
+        int rolls = 0;
+        for (int i = 1; i < arr.length - 1; i++)
+            for (int j = 1; j < arr[i].length - 1; j++)
+                if(accessible(i, j)) rolls++;
+        System.out.println(rolls);
+
+        int toRemove = remove();
+        int thisTime = toRemove;
+        while (thisTime > 0) {
+            thisTime = remove();
+            toRemove += thisTime;
+        }
+        System.out.println(toRemove);
+    }
+    public static int remove() {
+        int rolls = 0;
+        for (int i = 1; i < arr.length - 1; i++)
+            for (int j = 1; j < arr[i].length - 1; j++)
+                if(accessible(i, j)) {
+                    rolls++;
+                    remove(i, j);
+                }
+        return rolls;
+    }
+    public static void remove(int r, int c) {
+        arr[r][c] = ".";
+    }
+}
